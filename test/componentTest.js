7a9e3d96d68e55244956dd8d38460ba652e84a8f
Javey <jiawei23716@sina.com>
2016-04-08 19:07:43 +0800

    unit test

--- test/componentTest.js
+++ test/componentTest.js
@@ -35,4 +35,78 @@ describe('Component Test', function() {
         b.set('b', 3);
         b.element.outerHTML.should.be.eql('<b><a>2</a>3</b>');
     });
+
+    it('pass arguments to child', function() {
+        var C = Intact.extend({
+            defaults: {
+                c: 3
+            },
+            template: '<span><A a={this.get("c")} /></span>',
+            _init: function() {
+                this.A = A;
+            }
+        });
+        var html = '<span><a>3</a></span>';
+        var c = new C();
+        c.init();
+        c.element.outerHTML.should.be.eql(html);
+
+        C.prototype.template = '<span><A arguments={{a: this.get("c")}} /></span>';
+        c = new C();
+        c.init();
+        c.element.outerHTML.should.be.eql(html);
+    });
+
+    it('parent component update', function() {
+        var C = Intact.extend({
+            defaults: {
+                c: 3
+            },
+            template: '<span><A widget="a" a={this.get("c")} /></span>',
+            _init: function() {
+                this.A = A;
+            }
+        });
+        var html = '<span><a>3</a></span>';
+        var c = new C();
+
+        c.init();
+        c.set('c', 2);
+        c.element.outerHTML.should.be.eql('<span><a>2</a></span>');
+
+        c.widgets.a.set('a', 4);
+        c.element.outerHTML.should.be.eql('<span><a>4</a></span>');
+        c.update();
+        c.element.outerHTML.should.be.eql('<span><a>2</a></span>');
+    });
+
+    it('update child component', function() {
+        var C = Intact.extend({
+            defaults: {
+                component: undefined
+            },
+            template: '<div>{this.get("component")}</div>'
+        });
+        var c = new C(),
+            a = new A(),
+            b = new B(),
+            destroyAFn = sinon.spy(),
+            destroyBFn = sinon.spy();
+
+        c.init();
+        c.element.outerHTML.should.be.eql('<div></div>');
+
+        c.set('component', a);
+        a.inited.should.be.true;
+        a.rendered.should.be.true;
+        c.element.outerHTML.should.be.eql('<div><a>1</a></div>');
+
+        c.set('component', b);
+        b.inited.should.be.true;
+        b.rendered.should.be.true;
+        c.element.outerHTML.should.be.eql('<div><b><a>1</a>1</b></div>');
+
+        destroyAFn.called.should.be.false;
+        destroyBFn.called.should.be.false;
+    });
 });
