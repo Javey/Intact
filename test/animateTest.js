f00e161b1a5fafe4d3d872468225cfe56e3b9d2e
Javey <jiawei23716@sina.com>
2017-06-20 21:50:31 +0800

    Revert "Revert "animate""
    This reverts commit 3caa1b6ef3f4e602b808b8a16d4d940fd5da9e98.

--- test/animateTest.js
+++ test/animateTest.js
@@ -125,17 +125,17 @@ describe('Animate Test', function() {
         this.enableTimeouts(false);
         const app = Intact.mount(App, document.body);
         const C = Intact.extend({
-            template: `<ul>
+            template: `<Animate a:tag="ul">
                 <Animate a:tag="li" key="1">1</Animate> 
                 <Animate a:tag="li" key="2">2</Animate> 
-            </ul>`
+            </Animate>`
         });
         const D = Intact.extend({
-            template: `<ul>
+            template: `<Animate a:tag="ul">
                 <Animate a:tag="li" key="1">1</Animate> 
                 <Animate a:tag="li" key="3">3</Animate> 
                 <Animate a:tag="li" key="2">2</Animate> 
-            </ul>`
+            </Animate>`
         });
         app.load(C);
         app.load(D);
