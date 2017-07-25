5a75b5e1c0879959dc1269bfb3c421845b5251cc
Javey <jiawei23716@sina.com>
2017-07-25 19:22:01 +0800

    build

--- dist/intact.js
+++ dist/intact.js
@@ -1819,24 +1819,28 @@ function proxyEvent(e) {
 var addEventListener = void 0;
 var removeEventListener = void 0;
 if ('addEventListener' in doc) {
-    addEventListener = function addEventListener(name, fn) {
-        doc.addEventListener(name, fn, false);
+    addEventListener = function addEventListener(dom, name, fn) {
+        dom.addEventListener(name, fn, false);
     };
 
-    removeEventListener = function removeEventListener(name, fn) {
-        doc.removeEventListener(name, fn);
+    removeEventListener = function removeEventListener(dom, name, fn) {
+        dom.removeEventListener(name, fn);
     };
 } else {
-    addEventListener = function addEventListener(name, fn) {
-        doc.attachEvent("on" + name, fn);
+    addEventListener = function addEventListener(dom, name, fn) {
+        dom.attachEvent("on" + name, fn);
     };
 
-    removeEventListener = function removeEventListener(name, fn) {
-        doc.detachEvent("on" + name, fn);
+    removeEventListener = function removeEventListener(dom, name, fn) {
+        dom.detachEvent("on" + name, fn);
     };
 }
 
 var delegatedEvents = {};
+var unDelegatesEvents = {
+    'mouseenter': true,
+    'mouseleave': true
+};
 
 function handleEvent(name, lastEvent, nextEvent, dom) {
     if (name === 'blur') {
@@ -1845,22 +1849,31 @@ function handleEvent(name, lastEvent, nextEvent, dom) {
         name = 'focusin';
     }
 
-    var delegatedRoots = delegatedEvents[name];
+    if (!unDelegatesEvents[name]) {
+        var delegatedRoots = delegatedEvents[name];
 
-    if (nextEvent) {
-        if (!delegatedRoots) {
-            delegatedRoots = { items: new SimpleMap(), docEvent: null };
-            delegatedRoots.docEvent = attachEventToDocument(name, delegatedRoots);
-            delegatedEvents[name] = delegatedRoots;
-        }
-        delegatedRoots.items.set(dom, nextEvent);
-    } else if (delegatedRoots) {
-        var items = delegatedRoots.items;
-        if (items.delete(dom)) {
-            if (items.size === 0) {
-                removeEventListener(name, delegatedRoots.docEvent);
-                delete delegatedRoots[name];
+        if (nextEvent) {
+            if (!delegatedRoots) {
+                delegatedRoots = { items: new SimpleMap(), docEvent: null };
+                delegatedRoots.docEvent = attachEventToDocument(name, delegatedRoots);
+                delegatedEvents[name] = delegatedRoots;
             }
+            delegatedRoots.items.set(dom, nextEvent);
+        } else if (delegatedRoots) {
+            var items = delegatedRoots.items;
+            if (items.delete(dom)) {
+                if (items.size === 0) {
+                    removeEventListener(doc, name, delegatedRoots.docEvent);
+                    delete delegatedRoots[name];
+                }
+            }
+        }
+    } else {
+        if (lastEvent) {
+            removeEventListener(dom, name, lastEvent);
+        }
+        if (nextEvent) {
+            addEventListener(dom, name, nextEvent);
         }
     }
 }
@@ -1893,7 +1906,7 @@ function attachEventToDocument(name, delegatedRoots) {
             dispatchEvent(event, event.target, delegatedRoots.items, count, event.type === 'click');
         }
     };
-    addEventListener(name, docEvent);
+    addEventListener(doc, name, docEvent);
     return docEvent;
 }
 
@@ -3581,14 +3594,13 @@ Intact$1.prototype = {
                 _this3._triggerMountedQueue();
                 _this3.mount(null, vNode);
             });
+
             return dom;
         }
+
+        this._startRender = true;
         this.element = vdt.hydrate(this, dom, this.mountedQueue, this.parentDom, vNode);
         this.rendered = true;
-        if (this._pendingUpdate) {
-            this._pendingUpdate(null, vNode);
-            this._pendingUpdate = null;
-        }
         this.trigger('$rendered', this);
         this._create(null, vNode);
 
@@ -4043,35 +4055,15 @@ Intact$1.extend = function () {
  * @param node {Node} html节点
  */
 Intact$1.mount = function (Component, node) {
-    if (!Component || !(Component.prototype instanceof Intact$1 || Component === Intact$1)) {
-        throw new Error('expect for a class component');
-    }
-    var c = new Component();
-    c.parentDom = node;
-    // c._initMountedQueue();
-    var dom = void 0;
-    if (c.inited) {
-        dom = c.init();
-        // node.appendChild(dom);
-        c.mount();
-    } else {
-        c.one('$inited', function () {
-            dom = c.init();
-            // node.appendChild(dom);
-            c.mount();
-        });
-    }
-    return c;
+    var vNode = createVNode(Component);
+    render(vNode, node);
+    return vNode.children;
 };
 
 Intact$1.hydrate = function (Component, node) {
-    if (!Component || !(Component.prototype instanceof Intact$1 || Component === Intact$1)) {
-        throw new Error('expect for a class component');
-    }
-    var c = new Component();
-    c.parentDom = node;
-    c.hydrate(null, node.firstChild);
-    return c;
+    var vNode = createVNode(Component);
+    hydrateRoot(vNode, node);
+    return vNode.children;
 };
 
 var Animate = void 0;
