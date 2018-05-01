32e1ff8a6fdf4a450dfb9009ac0400a26abc50f1
Javey <jiawei23716@sina.com>
2018-05-01 12:33:48 +0800

    update index demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,21 +15,28 @@ export default class extends Layout {
             let $md = $($mds[i]);
             let md = $md.text();
             let html = marked.render(md);
-            // $md.replaceWith($(html));
-            $md.next('.code').html(html);
+            if ($md.hasClass('show')) {
+                $md.nextUntil('.code').next().html(html);
+            } else {
+                const $div = $('<div />').html(html);
+                const $codes = $div.find('pre code');
+                let template;
+                for (let i = 0; i < $codes.length; i++) {
+                    let $code = $($codes[i]);
+                    let code = $code.text();
+                    if ($code.hasClass('language-html')) {
+                        template = Intact.Vdt.compile(code);
+                    } else if ($code.hasClass('language-css')) {
+                        $code.parent().after(`<style>${code}</style>`);
+                    } else if ($code.hasClass('language-js')) {
+                        eval(code);
+                    }
+                }
+            } 
         }
         const $codes = $element.find('pre code');
-        let template;
         for (let i = 0; i < $codes.length; i++) {
             let $code = $($codes[i]);
-            let code = $code.text();
-            if ($code.hasClass('language-html')) {
-                template = Intact.Vdt.compile(code);
-            } else if ($code.hasClass('language-css')) {
-                $code.parent().after(`<style>${code}</style>`);
-            } else if ($code.hasClass('language-js')) {
-                eval(code);
-            }
             highlight.highlightBlock($code[0]);
         }
     }
