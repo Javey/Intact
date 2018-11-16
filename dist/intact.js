f1900103143942e74e0ecee27e6b25d5ed746a55
Javey <jiawei23716@sina.com>
2018-11-16 13:47:59 +0800

    chore: build for refactor of Event

--- dist/intact.js
+++ dist/intact.js
@@ -60,20 +60,7 @@ var indexOf = function () {
     }
 }();
 
-var nativeObject = Object.create;
-var createObject = function () {
-    if (nativeObject) {
-        return function (obj) {
-            return nativeObject(obj);
-        };
-    } else {
-        return function (obj) {
-            function Fn() {}
-            Fn.prototype = obj;
-            return new Fn();
-        };
-    }
-}();
+
 
 var SimpleMap = typeof Map === 'function' ? Map : function () {
     function SimpleMap() {
@@ -2429,90 +2416,43 @@ function directCloneChildren(children) {
     return children;
 }
 
-var ALL_PROPS = ["altKey", "bubbles", "cancelable", "ctrlKey", "eventPhase", "metaKey", "relatedTarget", "shiftKey", "target", "timeStamp", "type", "view", "which"];
-var KEY_PROPS = ["char", "charCode", "key", "keyCode"];
-var MOUSE_PROPS = ["button", "buttons", "clientX", "clientY", "layerX", "layerY", "offsetX", "offsetY", "pageX", "pageY", "screenX", "screenY", "toElement"];
-
-var rkeyEvent = /^key|input/;
-var rmouseEvent = /^(?:mouse|pointer|contextmenu)|click/;
-
-function Event(e) {
-    for (var i = 0; i < ALL_PROPS.length; i++) {
-        var propKey = ALL_PROPS[i];
-        this[propKey] = e[propKey];
-    }
-
-    if (!e.target) {
-        this.target = e.srcElement;
-    }
-
-    this._rawEvent = e;
+function preventDefault() {
+    this.returnValue = false;
 }
-Event.prototype.preventDefault = function () {
-    var e = this._rawEvent;
-    if (e.preventDefault) {
-        e.preventDefault();
-    } else {
-        e.returnValue = false;
-    }
-};
-Event.prototype.stopPropagation = function () {
-    var e = this._rawEvent;
-    e.cancelBubble = true;
-    e.stopImmediatePropagation && e.stopImmediatePropagation();
-};
 
-function MouseEvent(e) {
-    Event.call(this, e);
-    for (var j = 0; j < MOUSE_PROPS.length; j++) {
-        var mousePropKey = MOUSE_PROPS[j];
-        this[mousePropKey] = e[mousePropKey];
-    }
-}
-MouseEvent.prototype = createObject(Event.prototype);
-MouseEvent.prototype.constructor = MouseEvent;
-
-function KeyEvent(e) {
-    Event.call(this, e);
-    for (var j = 0; j < KEY_PROPS.length; j++) {
-        var keyPropKey = KEY_PROPS[j];
-        this[keyPropKey] = e[keyPropKey];
-    }
-}
-KeyEvent.prototype = createObject(Event.prototype);
-KeyEvent.prototype.constructor = KeyEvent;
-
-function proxyEvent(e) {
-    if (rkeyEvent.test(e.type)) {
-        return new KeyEvent(e);
-    } else if (rmouseEvent.test(e.type)) {
-        return new MouseEvent(e);
-    } else {
-        return new Event(e);
-    }
+function stopPropagation() {
+    this.cancelBubble = true;
+    this.stopImmediatePropagation && this.stopImmediatePropagation();
 }
 
 var addEventListener = void 0;
 var removeEventListener = void 0;
+function fixEvent(fn) {
+    return function (event) {
+        event.stopPropagation = stopPropagation;
+        if (!event.preventDefault) {
+            event.preventDefault = preventDefault;
+        }
+        fn(event);
+    };
+}
 if ('addEventListener' in doc) {
     addEventListener = function addEventListener(dom, name, fn) {
-        dom.addEventListener(name, fn, false);
+        fn._$cb = fixEvent(fn);
+        dom.addEventListener(name, fn._$cb, false);
     };
 
     removeEventListener = function removeEventListener(dom, name, fn) {
-        dom.removeEventListener(name, fn);
+        dom.removeEventListener(name, fn._$cb || fn);
     };
 } else {
     addEventListener = function addEventListener(dom, name, fn) {
-        fn.cb = function (e) {
-            e = proxyEvent(e);
-            fn(e);
-        };
-        dom.attachEvent("on" + name, fn.cb);
+        fn._$cb = fixEvent(fn);
+        dom.attachEvent('on' + name, fn._$cb);
     };
 
     removeEventListener = function removeEventListener(dom, name, fn) {
-        dom.detachEvent("on" + name, fn.cb || fn);
+        dom.detachEvent('on' + name, fn._$cb || fn);
     };
 }
 
@@ -2583,11 +2523,13 @@ function handleEvent(name, lastEvent, nextEvent, dom) {
     }
 }
 
-function dispatchEvent(event, target, items, count, isClick) {
+function dispatchEvent(event, target, items, count, isClick, eventData) {
     var eventToTrigger = items.get(target);
     if (eventToTrigger) {
         count--;
-        event.currentTarget = target;
+        eventData.dom = target;
+        // for fallback when Object.defineProperty is undefined
+        event._currentTarget = target;
         if (isArray(eventToTrigger)) {
             for (var i = 0; i < eventToTrigger.length; i++) {
                 var _eventToTrigger = eventToTrigger[i];
@@ -2598,7 +2540,7 @@ function dispatchEvent(event, target, items, count, isClick) {
         } else {
             eventToTrigger(event);
         }
-        if (event._rawEvent.cancelBubble) {
+        if (event.cancelBubble) {
             return;
         }
     }
@@ -2607,17 +2549,30 @@ function dispatchEvent(event, target, items, count, isClick) {
         if (isNullOrUndefined(parentDom) || isClick && parentDom.nodeType === 1 && parentDom.disabled) {
             return;
         }
-        dispatchEvent(event, parentDom, items, count, isClick);
+        dispatchEvent(event, parentDom, items, count, isClick, eventData);
     }
 }
 
 function attachEventToDocument(name, delegatedRoots) {
     var docEvent = function docEvent(event) {
         var count = delegatedRoots.items.size;
-        event || (event = window.event);
         if (count > 0) {
-            event = proxyEvent(event);
-            dispatchEvent(event, event.target, delegatedRoots.items, count, event.type === 'click');
+            var eventData = {
+                dom: doc
+            };
+            try {
+                Object.defineProperty(event, 'currentTarget', {
+                    configurable: true,
+                    get: function get() {
+                        return eventData.dom;
+                    }
+                });
+            } catch (e) {}
+            // ie8
+
+            // for compatibility
+            event._rawEvent = event;
+            dispatchEvent(event, event.target, delegatedRoots.items, count, event.type === 'click', eventData);
         }
     };
     addEventListener(doc, name, docEvent);
